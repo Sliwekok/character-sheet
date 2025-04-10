dae8d28827587e4e7fa7398893092571ee522eb5
Sliwekok <dominiksliwek@gmail.com>
2025-04-10 14:38:48 +0200

    added human race

---
+++ src/interfaces/Race/Human.ts
@@ -0,0 +1,14 @@
+import { Race } from '../Race';
+
+export const Human: Race = {
+    name: 'Human',
+    traits: ['Versatile', 'Adaptable'],
+    abilityModifiers: {
+        strength: 1,
+        dexterity: 1,
+        constitution: 1,
+        intelligence: 1,
+        wisdom: 1,
+        charisma: 1
+    }
+};
