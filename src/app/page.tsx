01008964527d63b38b8aa3a680281268dbf305b4
Sliwekok <dominiksliwek@gmail.com>
2025-04-12 15:55:24 +0200

    ui tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="items-center inline-flex">
+        <div className="scale-125 items-center inline-flex">
           <Image
             className="dark:invert"
             src="/01.png"
@@ -22,7 +22,7 @@ export default function Home() {
           </ol>
         </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="mt-5 scale-125 flex gap-4 items-center flex-col sm:flex-row">
           <Link
             className="rounded-full transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-(--fontcolor) font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="/home"
