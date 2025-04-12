ec85cde47cf3fb4159c4517ded16cc8a4ea11468
Sliwekok <dominiksliwek@gmail.com>
2025-04-13 01:03:40 +0200

    added shuffle with images on welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,51 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
+import { useWindowSize } from 'react-use';
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const { width, height } = useWindowSize();
+
+  const imagePaths = [
+    "/sketch/resized/1.png",
+    "/sketch/resized/2.png",
+    "/sketch/resized/3.png",
+    "/sketch/resized/4.png",
+    "/sketch/resized/5.png",
+    "/sketch/resized/6.png",
+    "/sketch/resized/7.png",
+    "/sketch/resized/8.png",
+  ];
+
+  function shuffleArray(array: string[]) {
+    return [...array].sort(() => Math.random() - 0.5);
+  }
+
+  const [shuffledImages, setShuffledImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    setShuffledImages(shuffleArray(imagePaths));
+  }, []);
+
+  const topImages = shuffledImages.slice(4, 8);
+  const bottomImages = shuffledImages.slice(0, 4);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen  pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
+      <div className="absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 grid grid-cols-4 gap-6 w-full px-8 opacity-40">
+        {topImages.map((src, i) => (
+            <Image key={i} src={src} alt={`sketch ${i}`} width={width/4} height={height/4} className="w-full h-auto scale-40" />
+        ))}
+      </div>
+
+      <div className="absolute bottom-10 left-1/2 -translate-x-1/2 grid grid-cols-4 gap-6 w-full px-8 opacity-40">
+        {bottomImages.map((src, i) => (
+            <Image key={i} src={src} alt={`sketch ${i}`} width={width/4} height={height/4} className="w-full h-auto scale-50" />
+        ))}
+      </div>
+
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="scale-125 items-center inline-flex">
           <Image
