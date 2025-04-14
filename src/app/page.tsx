d6fb65b2a1e1d29c405de73b324edb9af90a404d
Sliwekok <dominiksliwek@gmail.com>
2025-04-14 10:59:37 +0200

    styling welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,18 +36,18 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen  pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
       <div className="absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 grid grid-cols-4 gap-6 w-full px-8 opacity-40">
         {topImages.map((src, i) => (
-            <Image key={i} src={src} alt={`sketch ${i}`} width={width/4} height={height/4} className="w-full h-auto scale-40" />
+            <Image key={i} src={src} alt={`sketch ${i}`} width={width/4} height={height/4} className="w-full h-auto scale-40 select-none z-0" />
         ))}
       </div>
 
       <div className="absolute bottom-10 left-1/2 -translate-x-1/2 grid grid-cols-4 gap-6 w-full px-8 opacity-40">
         {bottomImages.map((src, i) => (
-            <Image key={i} src={src} alt={`sketch ${i}`} width={width/4} height={height/4} className="w-full h-auto scale-50" />
+            <Image key={i} src={src} alt={`sketch ${i}`} width={width/4} height={height/4} className="w-full h-auto scale-50 select-none z-0" />
         ))}
       </div>
 
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="scale-125 items-center inline-flex">
+        <div className="scale-125 items-center inline-flex backdrop-brightness-50 pr-5">
           <Image
             className="dark:invert"
             src="/01.png"
@@ -64,7 +64,7 @@ export default function Home() {
           </ol>
         </div>
 
-        <div className="mt-5 scale-125 flex gap-4 items-center flex-col sm:flex-row">
+        <div className="mt-5 scale-125 flex gap-4 items-center flex-col sm:flex-row ml-5">
           <Link
             className="rounded-full transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-(--fontcolor) font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="/home"
@@ -89,7 +89,7 @@ export default function Home() {
           </Link>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center z-20">
         <Link
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="/about"
